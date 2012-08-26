2474a1e25de882adaf5a97bb4545d42f2fdbbe41
S3308501 <ljxing_91@hotmail.com>
2012-08-27 09:36:20 +1000

    added

--- winestore.php
+++ winestore.php
@@ -1,123 +1,133 @@
+<!DOCTYPE HTML PUBLIC
+"-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html401/loose.dtd">
+
 <html>
-<title>Winestore</title>
+<head>
+   <title>Web Database Applications Part B - Wine Store Database</title>
+</head>
 <body>
-<form method="get" action="query.php" id="myform" >
-	Wine name: <input type="text" name="wname"id="wname"/><br/>
-	Wineryname: <input type="text" name="wyname" id="wyname" /><br/>
-	
-	
-  
-<?php 
-    $link = mysql_connect("127.0.0.1:3306","cuijinhe","cuijinhe");
-	$select_db=mysql_select_db("winestore",$link);
-	
-    $query=("select region_name from region");
-	$query1=("select variety from grape_variety");
-	$query2=("SELECT DISTINCT  `year` FROM  `wine` ORDER BY  `year` ");
-	$query3=("SELECT DISTINCT  `year` FROM  `wine` ORDER BY  `year` ");
-	$result=mysql_query($query);
-	$result1=mysql_query($query1);
-	$result2=mysql_query($query2);
-	$result3=mysql_query($query3);
-	echo "Region:";
-	echo " <select id='sel_region_name' name='region_name' >";
-	while ($row = mysql_fetch_array($result)) {
-	echo   "<option value='".$row["region_name"]."'>".$row["region_name"]."</option>";
-	}
-	echo	" </select><br/>";
-	
-	echo "Grape Variety:";
-	echo " <select id='sel_variety' name='variety'>";
-	echo "<option>All</option>";
-	while ($row=mysql_fetch_array($result1)){
-	echo "<option value='".$row["variety"]."'>".$row["variety"]."</option>";
-	}
-	echo	" </select><br/>";
-	
-	echo "Year:";
-	echo " <select id='year' name='year'>";
-	while ($row=mysql_fetch_array($result2)){
-	echo "<option value='".$row["year"]."'>".$row["year"]."</option>";
-	}
-	echo " </select>";
-	
-	echo "to";
-	echo " <select id='year1' name='year1'>";
-	while ($row=mysql_fetch_array($result3)){
-	echo "<option value='".$row["year"]."'>".$row["year"]."</option>";
-	}
-	echo	" </select><br/>";
-	
-	
-	mysql_close($link);
-	?> 
-	
-	Cost range ,minimum : <input type="text" name="minicost" value=""id="minicost"size="3"/>$ ---
-	            maximum : <input type="text" name="maxicost" value=""id="maxicost"size="3"/>$<br/>
-
-	Minimum number of wines in stock:<input type="text" name="instock" value=""id="instock"size="2" /><br/>
-	Minimum number of wines	ordered:<input type="text" name="ordered" value=""id="ordered"size="2" /><br/>		
-				
-
-
-	
-	</form>
-	<input type="button" onclick="checkFunction();" value="Search"/>
-	
-	
-	<script type="text/javascript">
-	function checkFunction()
-	{
-	
+   <form action="query.php" method="GET" id="my_form">
+      
+	  Wine Name: <input type="text" name="winename" id="winename" /><br /> 
+	  Winery Name: <input type="text" name="wineryname" id="wineryname" /><br />
+      
+	 <?php
 		
+		$connect = mysql_connect("yallara.cs.rmit.edu.au:55327", "winestore", "1234qwer");
+		$database = mysql_select_db("winestore", $connect);
 		
+		$query_region=("SELECT region_name from region");
+		$query_variety=("SELECT variety from grape_variety");
+		$query_year=("SELECT DISTINCT year from wine ORDER BY year ");
+		$query_year1=("SELECT DISTINCT year from wine ORDER BY year ");
 		
-		var i=document.getElementById("minicost").value.trim();
-		var j=document.getElementById("maxicost").value.trim();
-		if(i!=""&&j!=""&&i>=j)
+		$result=mysql_query($query_region);
+		$result2=mysql_query($query_variety);
+		$result3=mysql_query($query_year);
+		$result4=mysql_query($query_year1);
+		
+		echo "Region: ";
+		echo "<select id='region' name = 'region'>";
+		while($row = mysql_fetch_array($result))
 		{
-			alert("The minimaum price must lower than maximum!");
-		
-			return;
-			
+			echo "<option value ='".$row["region"]."'>".$row["region"]."</option>";
 		}
-		var c=document.getElementById("instock").value.trim();
-		var d=document.getElementById("ordered").value.trim();
-		if(c!=""&&d!=""&&c<1&&d<1)
+		echo "</select><br/>";
+		
+		echo "Grape Variety: ";
+		echo "<select id='variety' name = 'variety'>";
+		echo "<option> ALL </option>";
+		while($row = mysql_fetch_array($result2))
 		{
-		
-			alert("The number must equal or higher than 1");
-			return;
-		
-		
+			echo "<option value ='".$row["variety"]."'>".$row["variety"]."</option>";
 		}
-		var e=parseInt(document.getElementById("year").value.trim());
-		var f=parseInt(document.getElementById("year1").value.trim());
-		if(e>f)
+		echo "</select><br/>";
+	 
+		echo "Year: ";
+		echo "<select id='year' name ='year'>";
+		while($row = mysql_fetch_array($result3))
 		{
-			alert("The range of year msut appropriate");
-			
+			echo "<option value ='".$row["year"]."'>".$row["year"]."</option>";
+		}
+		echo "</select>";
+		
+		echo "to ";
+		echo "<select id='year1' name = 'year1>";
+		while($row = mysql_fetch_array($result4))
+		{
+			echo "<option value ='".$row["year"]."'>".$row["year"]."</option>";
+		}
+		echo "</select><br/>";
+		
+		mysql_close($connect);
+		?>
+      
+	  
+      Minimum number of stock: <input type="text" name="stock" id="stock" size="2" /><br />
+      
+	  Minimum number of wines ordered: <input type="text" name="order" id="order" size="2" /><br />
+	  
+	  Cost:<br/>
+		Min<input type="text" name="min_cost" id="min_cost" size="3" />
+		Max<input type="text" name="max_cost" id="max_cost" size="3" /><br />
+		
+      <input type="submit" value="Submit" onclick ="validation()" />
+   </form>
+   
+   <script type= "text/javascript">
+   function validation()
+   {
+		var a=document.getElementById("winename").value;
+		if(a==null || a=="")
+		{
+			alert("Enter wine name");
+			return false;
+		}
+		
+		var b=document.getElementById("wineryname").value;
+		if(b==null || b=="")
+		{
+			alert("Enter winery name");
+			return false;
+		}
+		
+		var c=document.getElementById("min_cost").value.trim();
+		var d=document.getElementById("max_cost").value.trim();
+		if( c!=""&& d!="" && c>=d)
+		{
+			alert("Maximum cost must be higher than minimum cost");
 			return;
 		}
 		
+		var e=document.getElementById("stock").value.trim();
+		var f=document.getElementById("order").value.trim();
+		if( e!=""&& f!="" && e < 1 && f < 1)
+		{
+			alert("The number must be greater or equal to 1");
+			return;
+		}
+		
+		var g=document.getElementById("year").value.trim();
+		var h=document.getElementById("year1").value.trim();
+		if (g==""&& h="" && c>=d)
+		{
+			alert("Select approriate range of years");
+			return;
+		}
 		
 		else
 		{
-		var form=document.getElementById("myform");
+			var form=document.getElementById("my_form");
+			
+			form.onsubmit = function(){};
+			form.submit();
+		}
 		
-	
-		 form.onsubmit=function(){};
-		 form.submit();
-		 
-			}
-	}
-	</script>
-
-
-
-
-
-
-
+		
+   }
+   </script>
+   
 </body>
 </html>
+
